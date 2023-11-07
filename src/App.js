7da997952466c189322e24fb9ef12ff266a27356
Mohamed Bougrinat <mohamed.bougrinat@lapiscine.pro>
2023-11-07 12:34:00 +0100

    Rajout LatestArticles

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Footer from './Footer';
+import Footer from "./Footer";
+import Header from "./Header";
+import LatestArticles from "./LatestArticles";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello la piscine</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>le lorem ne fonctionne pas ici c'est ouf !
-
-        </p>
-            <a href="">click ici si tu n'aS pas peur !</a>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header />
+      <LatestArticles />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+
 
---
+++ src/LatestArticles.js
@@ -0,0 +1,24 @@
+function LatestArticles() {
+    return (
+      <main>
+        <article>
+          <h2>First Title</h2>
+          <p>Lorem
+
+          </p>
+        </article>
+  
+        <article>
+          <h2>Second Titre</h2>
+          <p>Lorem</p>
+        </article>
+  
+        <article>
+          <h2>Third Title</h2>
+          <p>Lorem</p>
+        </article>
+      </main>
+    );
+  }
+  
+  export default LatestArticles;
